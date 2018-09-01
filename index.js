25a9acfd57cb2926e1a214f2fcff26b8b3dd364a
fonzaex <daniel.fnz@hotmail.com>
2018-08-31 22:17:56 -0300

    Git Ignore

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ var firebaseRef = firebase.database().ref('daniel');
 //GeoFire index
 var ref = new GeoFire(firebaseRef);
 
-var fishLocations = [
-    [-16.130262, 153.605347],   // Coral Sea
-    [-66.722541, -167.019653],  // Southern Ocean
-    [-41.112469, 159.054565],   // Tasman Sea
-    [30.902225, -166.66809]     // North Pacific Ocean
-  ];
-
-  // Set the initial locations of the fish in GeoFire
-  var promises = fishLocations.map(function(location, index) {
-    return geoFire.set("fish" + index, location).then(function() {
-      console.log("fish" + index + " initially set to [" + location + "]");
-    });
-  });
-
 
 
 
