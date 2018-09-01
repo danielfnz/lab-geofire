801ca347efdd91cf8911f97d3a84b0f97e38dab6
fonzaex <daniel.fnz@hotmail.com>
2018-08-31 22:35:18 -0300

    Adicionando localizacao no firebase

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var firebase = require('firebase');
 
 /* Inicializando o firebase com credenciais padroes do geofire */
 firebase.initializeApp({
-    apiKey: "AIzaSyC5IcRccDo289TTRa3Y7qJIu8YPz3EnKAI",
-    databaseURL: "https://geofire-9d0de.firebaseio.com"
-  });
+  apiKey: "AIzaSyC5IcRccDo289TTRa3Y7qJIu8YPz3EnKAI",
+  databaseURL: "https://geofire-9d0de.firebaseio.com"
+});
 
 /* Referencia do documento `daniel` no firebase */
-var firebaseRef = firebase.database().ref('daniel');
+var firebaseRef = firebase.database().ref('localizacoes');
 
 //GeoFire index
-var ref = new GeoFire(firebaseRef);
+var geoFire = new GeoFire(firebaseRef);
+
+//Adiciona a localização no firebase
+geoFire.set("estabelecimento", [-16.130262, 153.605347]);
 
 
 
